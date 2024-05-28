dadcf500fe3955108221f40aa18e6d3a6d0cc716
AxolotlJ-Dev <kalelcg.eg@gmail.com>
2024-05-28 01:39:59 -0600

    identificacion de recetas

--- models/Recipes.go
+++ models/Recipes.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type Recipes struct {
 	gorm.Model
 
+	FirstName    string `gorm:"not null" json:"first_name"`
+	LastName     string `gorm:"not null" json:"last_name"`
 	Title        string `gorm:"not null;unique_index" json:"title"`
 	Description  string `json:"description"`
 	Instructions string `json:"instructions"`
